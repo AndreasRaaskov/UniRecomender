f31a7c78058ed413362240ebdb80e3be78bd0e01
AndreasRaaskov <s183901@student.dtu.dk>
2023-06-08 10:12:50 +0200

    Update __init__.py

--- bank/__init__.py
+++ bank/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fc089b9218301ad987914c53481bff04'
 
-# set your own database
+# set your own 
 #db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'UIS'"
 db = "dbname='bank23010' user='postgres' host='127.0.0.1' password = '1234'"
 conn = psycopg2.connect(db)
